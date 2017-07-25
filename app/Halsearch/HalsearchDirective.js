82882b32f58e73b9e5116224001e4765027ebec4
anthonyguiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-07-25 14:50:12 +0200

    MOdifciation APP template

--- app/Halsearch/HalsearchDirective.js
+++
@@ -1,7 +0,0 @@
-app.directive('search', function() {
-  return {
-
-    templateUrl: 'wp-content/plugins/Widget_carto_hal/app/templates/search.html',//Template a afficher
-    controller:'searchctrl'//Controller a executer
-  };
-});
